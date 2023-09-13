af6088e7e3599b305f9687c90d05adc4065e3f02
Olayiwola Olanrewaju <larry_coal@outlook.com>
2023-09-13 16:23:04 -0400

    use appopriate header name

--- features/DashboardLayout.js
+++ features/DashboardLayout.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import Sidebar from './Sidebar';
-import styles from "./dashboard.module.css"
-const DashboardLayout = ({children}) => {
-    return (
-      <div className={styles.dashboardWrapper}>
-        <Sidebar />
-        <header>Home</header>
-        <div className={styles.pageContainer}>{children}</div>
-      </div>
-    );
+"use client"
+import React, { useEffect,useState } from "react";
+import Sidebar from "./Sidebar";
+import styles from "./dashboard.module.css";
+import { useRouter, usePathname } from "next/navigation";
+const DashboardLayout = ({ children }) => {
+  const [header, setHeader] = useState("Home");
+  const router = useRouter();
+  const pathname = usePathname()
+  useEffect(() => {
+    switch (pathname) {
+      case "/":
+        setHeader("Home");
+        break;
+      case "/linkedlist":
+        setHeader("LinkedList");
+        break;
+
+      default:
+        break;
+    }
+  }, [pathname]);
+  return (
+    <div className={styles.dashboardWrapper}>
+      <Sidebar />
+      <header>{header}</header>
+      <div className={styles.pageContainer}>{children}</div>
+    </div>
+  );
 };
 
 export default DashboardLayout;
